19ce9df310f2fadf584e682ebeb2a45ab7091fa0
minhkhoi204 <nvmkhoi20@gmail.com.com>
2023-11-24 14:53:19 +0700

    upload product model, controller, redux

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoute = require("./controller/userControllers.js");
 const shopRoute = require("./controller/shopController.js");
+const product = require("./controller/productController.js");
 
 const corsOptions = {
     origin: "http://localhost:3000",
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 
 app.use("/api/user", userRoute);
 app.use("/api/shop", shopRoute);
+app.use("/api/product", product);
 
 app.use(ErrorHandler);
 
