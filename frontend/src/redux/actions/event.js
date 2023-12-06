d95fc62781675bb560f629ab7b923b44800fbc98
Leonard <thienhuuhuynh.dev@gmailcom>
2023-12-06 22:53:50 +0700

    upload structure of redux

---
+++ frontend/src/redux/actions/event.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const createevent = () => async () => {
+
+};
+
+export const getAllEvent = () => () => {
+
+};
+
+export const deleteEvent = () => () => {
+
+};
