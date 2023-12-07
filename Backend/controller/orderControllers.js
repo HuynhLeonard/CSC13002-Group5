f0bee079c590c1490f945ed69f4d6addd6b633fa
letuanhtuan <ltatuan21@clc.fitus.edu.vn>
2023-12-07 15:11:26 +0700

    update full order controller

--- Backend/controller/orderControllers.js
+++ Backend/controller/orderControllers.js
@@ -101,6 +101,22 @@ router.put(
                     await updateOrder(o._id, o.qty);
                 });
             }
+
+            order.status = req.body.status;
+
+            if (req.body.status === "Delivered") {
+                order.deliveredAt = Date.now();
+                order.paymentInfo.status = "Succeeded";
+                const serviceCharge = order.totalPrice * .10;
+                await updateSellerInfo(order.totalPrice - serviceCharge);
+            }
+
+            await order.save({ validateBeforeSave: false });
+
+            res.status(200).json({
+                success: true,
+                order,
+            });
             async function updateOrder(id, qty) {
                 const product = await Product.findById(id);
 
@@ -122,4 +138,94 @@ router.put(
             return next(new ErrorHandler(error.message, 500));
         }
     })
-)
+);
+
+
+router.put(
+    "/order-refund/:id",
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+
+
+            const order = await Order.findById(req.params.id);
+
+            if (!order) {
+                return next(new ErrorHandler("Order not found with thid id", 500));
+            }
+            order.status = req.body.status;
+            await order.save({ validateBeforeSave: false });
+            res.status(200).json({
+                success: true,
+                order,
+                message: "Order Refund Request successfully."
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+
+    })
+);
+
+router.put(
+    "/order-refund-success/:id",
+    isSeller,
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const order = await Order.findById(req.params.id);
+
+            if (!order) {
+                return next(new ErrorHandler("Order not found with this id", 400));
+            }
+
+            order.status = req.body.status;
+
+            await order.save();
+
+            res.status(200).json({
+                success: true,
+                message: "Order Refund successfull!",
+            });
+
+            if (req.body.status === "Refund Success") {
+                order.cart.forEach(async (o) => {
+                    await updateOrder(o._id, o.qty);
+                });
+            }
+
+            async function updateOrder(id, qty) {
+                const product = await Product.findById(id);
+
+                product.stock += qty;
+                product.sold_out -= qty;
+
+                await product.save({ validateBeforeSave: false });
+            }
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+
+router.get(
+    "/admin-all-orders",
+    isAuthenticated,
+    isAdmin("Admin"),
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const orders = await Order.find().sort({
+                deliveredAt: -1,
+                createdAt: -1,
+            });
+            res.status(201).json({
+                success: true,
+                orders,
+            });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    })
+);
+
+
+module.exports = router;
