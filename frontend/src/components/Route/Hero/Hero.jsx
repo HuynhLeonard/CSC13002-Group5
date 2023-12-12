b7276745fefd89ccddf9134749470818c62d1534
Lucas-VTL <votanloc1106@gmail.com>
2023-12-12 23:07:40 +0700

    Hero updated

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -9,28 +9,28 @@ const Hero = () => {
       style={{
         backgroundImage:
           // "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
-          "url(https://mac-repairs-service.uk/wp-content/uploads/2015/08/MacBook-repais-banner.jpg)"
-          // "url(https://my-files.apjonlinecdn.com/landingpages/family-pages/z-by-hp/images/w100_grahics.png)"
+          // "url(https://mac-repairs-service.uk/wp-content/uploads/2015/08/MacBook-repais-banner.jpg)"
+          "url(https://my-files.apjonlinecdn.com/landingpages/family-pages/z-by-hp/images/w100_grahics.png)",
       }}
     >
-      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+      <div className={`${styles.section} w-[90%] 800px:w-[60%] ml-40`}>
         <h1
           className={`text-[30px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
           Best Collection for <br /> Technology Devices
         </h1>
-        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
-          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba] w-[50%]">
+          Get the chance to approach all the latest technology devices with the
+          cheapest price which ever existed
+          <br />
+          Don't be late and add new items to your cart now !!!
         </p>
         <Link to="/products" className="inline-block">
-            <div className={`${styles.button} mt-5`}>
-                 <span className="text-[#fff] font-[Poppins] text-[18px]">
-                    Shop Now
-                 </span>
-            </div>
+          <div className={`${styles.button} mt-5`}>
+            <span className="text-[#fff] font-[Poppins] text-[18px]">
+              Shop Now
+            </span>
+          </div>
         </Link>
       </div>
     </div>
