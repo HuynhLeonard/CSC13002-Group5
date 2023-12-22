cf366abbe64ab2963f33a1a087aaeaff79ff0793
Leonard <thienhuuhuynh.dev@gmailcom>
2023-12-22 19:42:31 +0700

    All UI in Shop Homepage

--- frontend/src/components/Shop/ShopCreate.jsx
+++ frontend/src/components/Shop/ShopCreate.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import styles from "../../styles/styles";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { toast } from "react-toastify";
-import { RxAvatar } from "react-icons/rx";
-import "../Css/Shop-regis.css";
+import React, {useState} from 'react';
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai";
+import styles from '../../styles/styles';
+import {Link, useNavigate} from "react-router-dom";
+import axios from 'axios';
+import {toast} from "react-toastify";
+import { RxAvatar } from 'react-icons/rx';
 
 const ShopCreate = () => {
   const navigate = useNavigate();
@@ -22,10 +21,10 @@ const ShopCreate = () => {
     e.preventDefault();
     const config = {
       headers: {
-        "Content-Type": "multipart/form-data",
-      },
+        "Content-Type": "multipart/form-data"
+      }
     };
-
+    
     // create form data
     const newForm = new FormData();
 
@@ -57,129 +56,203 @@ const ShopCreate = () => {
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
     setAvatar(file);
-  };
+  }
+
+
 
   return (
-    <div className="shop-reg-body">
-      <div className="shop-reg-wrapper">
-        <div className="shop-reg-content">
-          <div className="shop-reg-title">
-            <h1>Shop register</h1>
-            <div className="shop-reg-logo"></div>
-          </div>
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Register as a seller
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-[35rem]">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit}>
 
-          <form onSubmit={handleSubmit}>
-            <div className="shop-reg-name">
-              <i class="bx bx-user"></i>
-              <input
-                type="text"
-                name="name"
-                autoComplete="off"
-                required
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <span>Shop name</span>
-            </div>
-
-            <div className="shop-reg-phone-number">
-              <i class="bx bxs-phone"></i>
-              <input
-                type="text"
-                name="phone-number"
-                required
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-              <span>Phone number</span>
-            </div>
-
-            <div className="shop-reg-email">
-              <i class="bx bx-envelope"></i>
-              <input
-                type="email"
-                name="email"
-                autoComplete="off"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <span>Email address</span>
-            </div>
-
-            <div className="shop-reg-address">
-              <i class="bx bxs-home"></i>
-              <input
-                type="text"
-                name="address"
-                required
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-              <span>Address</span>
-            </div>
-
-            <div className="shop-reg-code">
-              <i class="bx bx-barcode-reader"></i>
-              <input
-                type="number"
-                name="zipcode"
-                required
-                value={zipCode}
-                onChange={(e) => setZipCode(e.target.value)}
-              />
-              <span>Zip code</span>
-            </div>
-
-            <div className="shop-reg-password">
-              <i class="bx bx-lock-open-alt"></i>
-              <input
-                type={visible ? "text" : "password"}
-                name="password"
-                autoComplete="current-password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {visible ? (
-                <AiOutlineEye
-                  className="shop-reg-eye"
-                  size={25}
-                  onClick={() => setVisible(false)}
+          <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Shop Name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="name"
+                  name="name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
-              ) : (
-                <AiOutlineEyeInvisible
-                  className="shop-reg-eye"
-                  size={25}
-                  onClick={() => setVisible(true)}
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Phone Number
+              </label>
+              <div className="mt-1">
+                <input
+                  type="number"
+                  name="phone-number"
+                  required
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
-              )}
-              <span>Password</span>
+              </div>
             </div>
 
-            <div className="shop-reg-file">
-              <input
-                type="file"
-                name="avatar"
-                id="file-input"
-                accept=".jpg,.jpeg,.png"
-                onChange={handleFileInputChange}
-              />
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
             </div>
 
-            <div className="shop-reg-btn">
-              <input type="submit" value="Submit" />
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Address
+              </label>
+              <div className="mt-1">
+                <input
+                  type="address"
+                  name="address"
+                  required
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Zip Code
+              </label>
+              <div className="mt-1">
+                <input
+                  type="number"
+                  name="zipcode"
+                  required
+                  value={zipCode}
+                  onChange={(e) => setZipCode(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1 relative">
+                <input
+                  type={visible ? "text" : "password"}
+                  name="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+                {visible ? (
+                  <AiOutlineEye
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(false)}
+                  />
+                ) : (
+                  <AiOutlineEyeInvisible
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(true)}
+                  />
+                )}
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium text-gray-700"
+              ></label>
+              <div className="mt-2 flex items-center">
+                <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                  {avatar ? (
+                    <img
+                      src={URL.createObjectURL(avatar)}
+                      alt="avatar"
+                      className="h-full w-full object-cover rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
+                </span>
+                <label
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.jpeg,.png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                Submit
+              </button>
+            </div>
+            <div className={`${styles.noramlFlex} w-full`}>
+              <h4>Already have an account?</h4>
+              <Link to="/shop-login" className="text-blue-600 pl-2">
+                Sign in
+              </Link>
             </div>
           </form>
-
-          <div className="shop-reg-sign-up">
-            Already have an count?
-            <Link to="/shop-login"> Sign in</Link>
-          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ShopCreate;
+export default ShopCreate
--- frontend/src/components/Shop/ShopLogin.jsx
+++ frontend/src/components/Shop/ShopLogin.jsx
@@ -5,7 +5,6 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 // import { server } from "../../server";
 import { toast } from "react-toastify";
-import "../Css/Shop-login.css";
 
 const ShopLogin = () => {
   const navigate = useNavigate();
@@ -35,70 +34,105 @@ const ShopLogin = () => {
   };
 
   return (
-    <div className="shop-log-body">
-      <div className="shop-log-wrapper">
-        <div className="shop-log-content">
-          <div className="shop-log-title">
-            <h1>Shop login</h1>
-            <div className="shop-log-logo"></div>
-          </div>
-          <form method="post" onSubmit={handleSubmit}>
-            <div className="shop-log-email">
-              <i class="bx bx-envelope"></i>
-              <input
-                type="email"
-                name="email"
-                autoComplete="off"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <span>Email</span>
-            </div>
-            <div className="shop-log-password">
-              <i class="bx bx-lock-open-alt"></i>
-              <input
-                type={visible ? "text" : "password"}
-                name="password"
-                autoComplete="current-password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {visible ? (
-                <AiOutlineEye
-                  className="shop-log-eye"
-                  size={25}
-                  onClick={() => setVisible(false)}
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Login to your shop
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
-              ) : (
-                <AiOutlineEyeInvisible
-                  className="shop-log-eye"
-                  size={25}
-                  onClick={() => setVisible(true)}
-                />
-              )}
-              <span>Password</span>
-            </div>
-            <div className="shop-log-function">
-              <div className="shop-log-remember">
-                <input type="checkbox" name="remember-me" id="remember-me" />
-                <label for="remember-me">Remember me</label>
-              </div>
-              <div className="shop-log-forgot">
-                <a href=".forgot-password">Forgot your password?</a>
               </div>
             </div>
-            <div className="shop-log-submit">
-              <input type="submit" value="Log in"></input>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1 relative">
+                <input
+                  type={visible ? "text" : "password"}
+                  name="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+                {visible ? (
+                  <AiOutlineEye
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(false)}
+                  />
+                ) : (
+                  <AiOutlineEyeInvisible
+                    className="absolute right-2 top-2 cursor-pointer"
+                    size={25}
+                    onClick={() => setVisible(true)}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex} justify-between`}>
+              <div className={`${styles.noramlFlex}`}>
+                <input
+                  type="checkbox"
+                  name="remember-me"
+                  id="remember-me"
+                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div className="text-sm">
+                <a
+                  href=".forgot-password"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                Submit
+              </button>
+            </div>
+            <div className={`${styles.noramlFlex} w-full`}>
+              <h4>Not have any account?</h4>
+              <Link to="/shop-create" className="text-blue-600 pl-2">
+                Sign Up
+              </Link>
             </div>
           </form>
-          <div className="shop-log-sign-up">
-            <h4>Not have any account?</h4>
-            <Link to="/shop-create" className="text-blue-600 pl-2">
-              Sign up
-            </Link>
-          </div>
         </div>
       </div>
     </div>
