3296e15ee4e98f6d03580cb5ed27f8c59b25cbd0
minhkhoi204 <nvmkhoi20@gmail.com.com>
2024-01-09 11:08:05 +0700

    redux: event, product,...

--- frontend/src/redux/actions/event.js
+++ frontend/src/redux/actions/event.js
@@ -1,13 +1,85 @@
 import axios from 'axios';
 
-export const createevent = () => async () => {
+export const createEvent = (newForm) => async (dispatch) => {
+    try{
+        dispatch({
+            type: "eventCreateRequest"
+        });
 
+        const config = {headers: {"Content-Type":"multipart/form-data"}};
+        
+        const {data} = await axios.post(`https://localhost:8000/api/event/create-event`, newForm, config,);
+
+        dispatch({
+            type: "eventCreateSuccess",
+            payload: data.event
+        })
+    } catch (error){
+        dispatch({
+            type: "eventCreateFail",
+            payload: error.response.data.message
+        });
+    }
 };
 
-export const getAllEvent = () => () => {
+export const getAllEventsShop = (id) => async (dispatch) => {
+    try {
+      dispatch({
+        type: "getAlleventsShopRequest",
+      });
 
+      const { data } = await axios.get(
+        `http://localhost:8000/api/event/get-all-events/${id}`
+      );
+      dispatch({
+        type: "getAlleventsShopSuccess",
+        payload: data.events,
+      });
+    } catch (error) {
+      dispatch({
+        type: "getAlleventsShopFailed",
+        payload: error.response.data.message,
+      });
+    }
 };
 
-export const deleteEvent = () => () => {
+export const deleteEvent = (id) => async (dispatch) => {
+    try {
+      dispatch({
+        type: "deleteeventRequest",
+      })
 
-};
+      const {data} = await axios.delete(`http://localhost:8000/api/event/delete-shop-event/${id}`,{
+        withCredentials: true,
+      });
+
+      dispatch({
+        type: "deleteeventSuccess",
+        payload: data.message,
+      });
+    } catch (error) {
+      dispatch({
+        type: "deleteeventFailed",
+        payload: error.response.data.message,
+      });
+    }
+  }
+
+export const getAllEvents = () => async (dispatch) => {
+    try {
+    dispatch({
+    type: "getAlleventsRequest",
+    });
+
+    const {data} = await axios.get(`https://localhost:8000/api/event/get-all-events`);
+    dispatch({
+    type: "getAlleventsSuccess",
+    payload: data.events,
+    });
+    } catch (error) {
+        dispatch({
+        type: "getAlleventsFailed",
+        payload: error.response.data.message,
+        });
+    }
+}
